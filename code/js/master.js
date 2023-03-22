cc63c12d90c444b737419a983926b135e0514da4
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2023-03-22 15:25:32 +0800

    test

--- code/js/master.js
+++ code/js/master.js
@@ -1,4 +1,71 @@
 
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+  loader.style.display = "none";
+
+  console.log("Welcome!");
+})
 
 
 
+
+function charBoxResize() {
+  const set = document.querySelectorAll(".char");
+
+  for (i = 0; i < set.length; i++) {
+    console.log("width " + set[i].offsetWidth);
+
+    set[i].style.height = set[i].offsetWidth + "px";
+  }
+}
+
+
+function charBoxScale() {
+  const set = document.querySelectorAll(".char-box");
+
+  for (i = 0; i < set.length; i++) {
+    const initialWidth = getComputedStyle(set[i]).getPropertyValue('--char');
+    console.log(initialWidth);
+
+    const currentWidth = set[i].offsetWidth;
+    console.log(currentWidth);
+
+    var scaleNumber = currentWidth / initialWidth;
+
+    set[i].style.transform = "scale(" + scaleNumber + ")"
+  }
+}
+
+
+function resbox() {
+  console.log("resbox\(\) is ready");
+
+  var set = document.querySelectorAll(".resbox");
+
+  for (i = 0; i < set.length; i++) {
+    var mom = set[i].parentElement;
+
+    var momInitialW = getComputedStyle(mom).getPropertyValue('--ill-w');
+
+    var momCurrentW = mom.offsetWidth;
+
+    console.log("🤖️ initial height: " + momInitialW);
+
+    var scaleRatio = momCurrentW / momInitialW;
+
+    console.log("🤖️ scaleRatio: " + scaleRatio);
+
+    set[i].style.transform = "scale(" + scaleRatio + ")";
+  }
+}
+
+
+window.addEventListener("load",function(){
+  // charBoxScale();
+  resbox();
+},false);
+
+window.addEventListener("resize",function(){
+  // charBoxScale();
+  resbox();
+},false);
